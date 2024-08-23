2cebe39950639f775f258a3219fa115ca5ed2325
donald <donald.cameron@qa.com>
2024-08-23 13:21:38 +0100

    Fixed copy/pasted code in weather_data.py Added python_digital_review.py to review learning points from DIGITAL Added Folders for Adv_Collections Added Folders for User Functions Added Folders for File Interaction Added Folders for Regex Not Pushing often enough! Added Lab Solutions folder Modified demo_regex programs and removed functions Completed OOP Directory and demos and exercises Completed Modules & Packages demos
    Updated Comments in all programs.
    Created Aref demo with hello world program
    Removed Aref demo program
    Updated List comprehension
    Fixed cinemagoer version
    Added return None to demo_unit

--- 7_Modules_And_Packages/demo_unittest_calc.py
+++ 7_Modules_And_Packages/demo_unittest_calc.py
@@ -13,12 +13,15 @@ from demo_calculator import add, multiply, divide
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(7, add(4, 3))
+        return None
 
     def test_multiply(self):
         self.assertEqual(12, multiply(4, 3))
+        return None
 
     def test_divide(self):
         self.assertEqual('1.333', divide(4, 3))
+        return None
 
 
 if __name__ == "__main__":
